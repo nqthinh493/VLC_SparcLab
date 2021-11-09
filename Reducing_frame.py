51605333c0bf4b46142b1f20cd66e8b38c9804d0
chudinhhai <93870832+chudinhhai@users.noreply.github.com>
2021-11-09 10:43:53 +0700

    Code for reducing frames

---
+++ Reducing_frame.py
@@ -0,0 +1,13 @@
+import cv2
+vidcap = cv2.VideoCapture('test.mp4')
+success,image = vidcap.read()
+count = 0
+while success:
+  if(count%4==0):
+    cv2.imwrite("frame%d.jpg" % count, image)
+
+  success,image = vidcap.read()
+
+  print('Read a new frame: ', success)
+  count += 1
+
