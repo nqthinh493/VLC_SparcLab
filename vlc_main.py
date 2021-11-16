3ffe98d1410b7a71b730ed6d64174da6e11d9ba8
NQ. Thinh <nqthinh493@gmail.com>
2021-11-17 00:21:19 +0800

    add filter

--- vlc_main.py
+++ vlc_main.py
@@ -11,10 +11,49 @@ os.path.abspath(os.getcwd())
 def column(matrix, i):
     return [row[i] for row in matrix]
 
+def medfilt(x, k):
+    """Apply a length-k median filter to a 1D array x.
+    Boundaries are extended by repeating endpoints.
+    """
+    import numpy as np
+
+    assert k % 2 == 1, "Median filter length must be odd."
+    assert x.ndim == 1, "Input must be one-dimensional."
+    
+    k2 = (k - 1) // 2
+    y = np.zeros ((len (x), k), dtype=x.dtype)
+    y[:,k2] = x
+    for i in range (k2):
+        j = k2 - i
+        y[j:,i] = x[:-j]
+        y[:j,i] = x[0]
+        y[:-j,-(i+1)] = x[j:]
+        y[-j:,-(i+1)] = x[-1]
+    return np.median (y, axis=1)
+
+def meanfilt(x, k):
+    """Apply a length-k mean filter to a 1D array x.
+    Boundaries are extended by repeating endpoints.
+    """
+    
+    import numpy as np
+
+    assert k % 2 == 1, "Median filter length must be odd."
+    assert x.ndim == 1, "Input must be one-dimensional."
+    
+    k2 = (k - 1) // 2
+    y = np.zeros((len(x), k), dtype=x.dtype)
+    y[:,k2] = x
+    for i in range(k2):
+        j = k2 - i
+        y[j:,i] = x[:-j]
+        y[:j,i] = x[0]
+        y[:-j,-(i+1)] = x[j:]
+        y[-j:,-(i+1)] = x[-1]
+    return np.mean(y, axis=1)
 
 for i in classes:
     path = os.path.join(directory,i)
-    
     for img in os.listdir(path):
         img_array = cv2.imread(os.path.join(path, img), cv2.IMREAD_GRAYSCALE)
         thresh = 10  #0: black 255: white
@@ -54,23 +93,28 @@ for i in classes:
         
         # Plot 1
         plt.figure(figsize=(20, 11))
+        # plt.subplot(2, 2, 1)
+        # plt.title("Origin Image")
+        # plt.imshow(img_org)
+        # plt.axvline(x = k, color = 'r', linestyle = '--')
         plt.subplot(2, 2, 1)
-        plt.title("Origin Image")
-        plt.imshow(img_org)
-        plt.axvline(x = k, color = 'r', linestyle = '--')
-        plt.subplot(2, 2, 2)
         plt.title("Gray Image")
  
         plt.imshow(imgGray, cmap= 'gray')
         plt.plot(B, y)
         plt.plot(B*coefficent, y)
-        plt.subplot(2, 2, 3)
-        plt.title("Histogram average of column")
-        plt.plot(average_line_oy, y)
-        
-        plt.subplot(2, 2, 4)
+        plt.subplot(2, 2, 2)
+
         plt.title("Histogram at column " + str(k))
         plt.plot(B, y)
+        plt.subplot(2, 2, 3)
+        plt.title("apply median filter")
+        plt.plot(medfilt(B,3), y)
+        plt.subplot(2, 2, 4)
+        plt.title("apply mean filter")
+        plt.plot(meanfilt(medfilt(B,3),3), y)
+        
+
            
             
         plt.show()
